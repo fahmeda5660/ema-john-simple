a8c031af877b94ede58af1a076664e6506de6847
fahmeda5660 <fariha.208860@gmail.com>
2023-03-29 13:49:46 +0600

    simple2 done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,9 +8,20 @@ const Cart = ({cart}) => {
 
     let totalPrice = 0;
     let totalShipping =0;
+    let quantity = 0;
     for(const product of cart){
-        totalPrice = totalPrice+product.price;
+        // price depend on quantity
+        // short cuts:1 
+        // product.quantity = product.quantity || 1 ;
+        // short cuts:2
+        if(product.quantity ===0){
+            product.quantity = 1; 
+        } 
+        // tough solution
+
+        totalPrice = totalPrice+product.price * product.quantity;
         totalShipping =totalShipping+product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = totalPrice*7/100;
     const grandTotal = totalPrice+totalShipping+tax;
@@ -18,6 +29,7 @@ const Cart = ({cart}) => {
         <div className='cart'>
             <h4>Order Summary</h4>
             <p>Selected Items: {cart.length}</p>
+            <p>Quantity: {quantity}</p>
             <p>Total Price:${totalPrice}</p>
             <p>Total Shipping:${totalShipping}</p>
             <p>Tax:{tax.toFixed(2)}</p>
